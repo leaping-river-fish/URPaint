5fa4a53d1b610ae15c384bef95fc9dbb4c04dfac
leaping-river-fish <nickzheng81@gmail.com>
2025-08-25 12:48:04 -0400

    jwt token testing

---
+++ go-middleware/internal/middleware/jwt.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func JWTAuth(secret []byte, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing Authorization Header", http.StatustUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ") 
+		token, err := jwt.Parse(tokenString, func(token, *jwt.Token) (interface{}, error){
+			return secret, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
